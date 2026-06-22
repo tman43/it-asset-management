66ceaac3703532c3a6dfebd3ac55f1e0f8678297
tman43 <140545363+tman43@users.noreply.github.com>
2026-06-22 12:04:46 +0200

    Backend: Add REST controller classes

---
+++ backend/src/main/java/com/itassets/controller/AssetController.java
@@ -0,0 +1,10 @@
+package com.itassets.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assets")
+public class AssetController {
+    // TODO: Implement asset management endpoints
+}
