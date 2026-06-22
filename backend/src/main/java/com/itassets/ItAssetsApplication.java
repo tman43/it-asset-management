ea88ad00e6da17a433c375712646a014e96c51a3
tman43 <140545363+tman43@users.noreply.github.com>
2026-06-22 12:03:54 +0200

    Backend: Add main application and configuration classes

---
+++ backend/src/main/java/com/itassets/ItAssetsApplication.java
@@ -0,0 +1,12 @@
+package com.itassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAssetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItAssetsApplication.class, args);
+    }
+}
